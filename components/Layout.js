4b352d5889c48a8f6778fedc0a94b7d72fb11662
Big Choi <4bigchoi23@gmail.com>
2020-12-21 14:13:56 +0900

    update Layout components

--- components/Layout.js
+++ components/Layout.js
@@ -1,56 +1,76 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import { useSession, signin, signout } from 'next-auth/client'
+import { Dropdown } from 'react-bootstrap';
 
 export default function Layout(props) {
   const [ session, loading ] = useSession()
+  const image = session
+    ? (session.user.image ? session.user.image : process.env.sneak.image)
+    : process.env.guest.image
 
   return (
     <>
       <Head>
-        <title>Keep</title>
+        <title>{process.env.title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <div id="app">
-        <header>
-          <div className="container d-flex justify-content-between">
-            <h1><Link href="/"><a>Keep</a></Link></h1>
+        {session && <>
+        </>}
+        <header className="header">
+          <div className="container-lg d-flex justify-content-between align-items-center">
+            <h1 className="brand">
+              <Link href="/">
+                <a>
+                  <span className="brand-logo"></span>
+                  {process.env.title}
+                </a>
+              </Link>
+            </h1>
             <div>
               {!session && <>
-                Not signed in <br/>
                 <button
                   type="button"
                   className="btn btn-sm btn-primary"
                   onClick={signin}
                 >
-                  <i className="fa fa-user-circle-o mr-2"></i>
+                  <i className="fa fa-user-circle-o mr-2" aria-hidden="true"></i>
                   <span>Sign in</span>
                 </button>
               </>}
               {session && <>
-                Signed in as {session.user.email} <br/>
-                {session.user.id} <br/>
-                <button
-                  type="button"
-                  className="btn btn-sm btn-danger"
-                  onClick={signout}
-                >
-                  <i className="fa fa-lock mr-2" aria-hidden="true"></i>
-                  <span>Sign out</span>
-                </button>
+                <div className="d-inline-block">
+                  <Dropdown>
+                    <Dropdown.Toggle variant="" bsPrefix="no-caret" id="dropdown-basic" className="p-0 shadow-none">
+                      <img src={image} className="rounded-circle" width="24" />
+                    </Dropdown.Toggle>
+
+                    <Dropdown.Menu>
+                      <Dropdown.Item href="/settings/profile">Settings</Dropdown.Item>
+                      <Dropdown.Item href="/" onClick={signout}>Sign out</Dropdown.Item>
+                    </Dropdown.Menu>
+                  </Dropdown>
+                </div>
               </>}
             </div>
           </div>
         </header>
-        <main>
+
+        <main className={session ? 'main' : ''}>
           {props.children}
         </main>
-        <footer>
-          <div className="container">
-            <div className="text-center p-3">&copy; 2020. All rights reserved.</div>
+
+        {session && <>
+        </>}
+        <footer className="footer">
+          <div className="container-lg">
+            <div className="text-center p-3">&copy; {(new Date().getFullYear())}. {process.env.title}. All rights reserved.</div>
           </div>
         </footer>
       </div>
+      {/* <style jsx>{``}</style> */}
     </>
   )
 }
