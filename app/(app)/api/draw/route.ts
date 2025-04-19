32d701a6594cba3bc80aa7b093ee57d13ba03e61
Big+ <4bigchoi23@gmail.com>
2025-04-19 20:29:41 +0900

    fix: api/draw url validation (#11)
    * fix: api/draw url validation
    * fix: api/draw change `fetch()` data cache option

--- app/(app)/api/draw/route.ts
+++ app/(app)/api/draw/route.ts
@@ -9,8 +9,8 @@ export async function GET(request: NextRequest) {
   let description = '';
   let image = '';
 
-  if (q) {
-    const data = await fetch(q);
+  if (q.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)) {
+    const data = await fetch(q, { cache: 'no-store' });
     const html = await data.text();
     const dom = new JSDOM(html);
     const doc = dom?.window?.document;
