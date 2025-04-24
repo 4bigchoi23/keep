a76afe9aa843668a1ce11b4ae97fe3f5519a64b0
Big+ <4bigchoi23@gmail.com>
2025-04-24 10:03:39 +0900

    fix: extract youtube video data (#16)
    * fix: extract youtube video data
    * fix: activate with google token
    * fix: allow only active session user

--- app/(app)/api/draw/route.ts
+++ app/(app)/api/draw/route.ts
@@ -1,4 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
+import { auth } from "@/auth";
 import { JSDOM } from 'jsdom';
 
 export async function GET(request: NextRequest) {
@@ -10,6 +11,7 @@ export async function GET(request: NextRequest) {
   let image = '';
 
   if (q.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)) {
+    const session = await auth();
     const data = await fetch(q, { cache: 'no-store' });
     const html = await data.text();
     const dom = new JSDOM(html);
@@ -18,6 +20,34 @@ export async function GET(request: NextRequest) {
     title = doc?.querySelector('title')?.textContent ?? '';
     description = doc?.querySelector('meta[property="og:description"]')?.getAttribute('content') ?? '';
     image = doc?.querySelector('meta[property="og:image"]')?.getAttribute('content') ?? '';
+
+    const u = new URL(q);
+    if (['www.youtube.com', 'youtu.be'].includes(u?.host ?? '') && (!title || !description || !image)) {
+      let v = '';
+      switch (u?.host) {
+        case 'www.youtube.com':
+          v = u?.searchParams?.get('v') ?? '';
+          break;
+        case 'youtu.be':
+          v = u?.pathname?.split('/')?.[1] ?? '';
+          break;
+      }
+
+      if (v && session && process.env.GOOGLE_TOKEN) {
+        const tube = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${v}&key=${process.env.GOOGLE_TOKEN}`);
+        const json = await tube.json();
+        const snippet = json?.items?.[0]?.snippet;
+
+        title = snippet?.title ?? title;
+        description = snippet?.description ?? description;
+        image = snippet?.thumbnails?.maxres?.url 
+          ?? snippet?.thumbnails?.standard?.url 
+          ?? snippet?.thumbnails?.high?.url 
+          ?? snippet?.thumbnails?.medium?.url 
+          ?? snippet?.thumbnails?.default?.url 
+          ?? image;
+      }
+    }
   }
 
   return NextResponse.json(
