b4a2e32cb68a0e6067bc1447c58364d923a1ecf1
Big+ <4bigchoi23@gmail.com>
2025-04-24 10:00:35 +0900

    fix: allow only active session user

--- app/(app)/api/draw/route.ts
+++ app/(app)/api/draw/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from 'next/server';
-// import { auth } from "@/auth";
+import { auth } from "@/auth";
 import { JSDOM } from 'jsdom';
 
 export async function GET(request: NextRequest) {
@@ -11,6 +11,7 @@ export async function GET(request: NextRequest) {
   let image = '';
 
   if (q.match(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/)) {
+    const session = await auth();
     const data = await fetch(q, { cache: 'no-store' });
     const html = await data.text();
     const dom = new JSDOM(html);
@@ -21,9 +22,7 @@ export async function GET(request: NextRequest) {
     image = doc?.querySelector('meta[property="og:image"]')?.getAttribute('content') ?? '';
 
     const u = new URL(q);
-    if (['www.youtube.com', 'youtu.be'].includes(u?.host ?? '')) {
-      // const session = await auth();
-
+    if (['www.youtube.com', 'youtu.be'].includes(u?.host ?? '') && (!title || !description || !image)) {
       let v = '';
       switch (u?.host) {
         case 'www.youtube.com':
@@ -34,7 +33,7 @@ export async function GET(request: NextRequest) {
           break;
       }
 
-      if (v && process.env.GOOGLE_TOKEN) {
+      if (v && session && process.env.GOOGLE_TOKEN) {
         const tube = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&id=${v}&key=${process.env.GOOGLE_TOKEN}`);
         const json = await tube.json();
         const snippet = json?.items?.[0]?.snippet;
