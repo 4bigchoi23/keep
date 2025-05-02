4c6b5ce826e245a8212824c310ba1daddceda1e8
Big+ <4bigchoi23@gmail.com>
2025-05-02 17:50:36 +0900

    refactor: layouts

--- app/(cms)/layout.tsx
+++ app/(cms)/layout.tsx
@@ -1,33 +1,17 @@
-import type { Metadata } from "next";
-export const metadata: Metadata = {
-  title: "KEEP",
-  description: "Keep your bookmark!",
-};
+// import type { Metadata } from 'next';
+// export const metadata: Metadata = {
+//   title: "",
+//   description: "",
+// };
 
-import "../globals.css";
-import { Toaster } from "@/components/ui/sonner"
-
-export default async function RootLayout({
+export default async function CMSLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
-      <body>
-        <div id="app">
-          {children}
-        </div>
-
-        <Toaster 
-          position="top-center" 
-          toastOptions={{
-            classNames: {
-              toast: 'font-body',
-            },
-          }} 
-        />
-      </body>
-    </html>
+    <>
+      {children}
+    </>
   );
 }
